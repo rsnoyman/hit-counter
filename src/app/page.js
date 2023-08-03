df47bdf560a821152fe2c86b8484799fe69f2c1f
Ryan Snoyman <rsnoyman@atlassian.com>
2023-08-03 14:34:09 +1000

    docs

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,6 @@ import { readFile, writeFile } from "../helpers/file-helpers";
 
 const DATABASE_PATH = "/src/database.json";
 
-/*
-async functions
-
-`readFile` takes 1 argument:
-• the path to the file:
-
-readFile('/path/to/file');
-
-`writeFile` takes 2 arguments:
-• The path to the file
-• The new contents for the file
-
-writeFile(
-  '/path/to/file',
-  '{ "hello": "world" }'
-);
-*/
-
 function Home() {
   // TODO
 
